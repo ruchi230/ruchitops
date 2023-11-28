18dd1d375e8b9a57caacb9c61f9a7388887b63a8
ruchi230 <136887290+ruchi230@users.noreply.github.com>
2023-11-28 11:44:13 +0530

    Add files via upload

---
+++ assignment/MVC .NET 3/5/pattern1/pattern1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the number : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();    
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignment/MVC .NET 3/5/pattern3/pattern3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the number : ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(" # ");
+                    }
+                    else
+                    {
+                        Console.Write(" * ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
